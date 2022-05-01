1edb9ac706284d85d3676deaa9dd553849fbae30
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-05-01 14:05:02 +1000

    added imports and normalize.css

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,3 +1,6 @@
+
+
+
 export default function Contact() {
     return (
         <div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Styles
+import 'normalize.css'
 //Components
 import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
 
 export const App = () => {
     return (
         <div>
             <h1>Dev Portfolio</h1>
             <About />
+            <Contact />
+            <Projects />
             </div>
       
     )
