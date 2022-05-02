9953311804d056796c297d7393bac3c4b86e7e79
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-05-02 14:44:10 +1000

    setup desktop view css for about content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './about.scss';
 import '../../grid.scss';
+import aboutImage from '../../images/about/about.jpg';
 
 const About = () => {
   return (
@@ -23,20 +24,7 @@ const About = () => {
           </p>
         </div>
         <div class="about__img container__col-6">
-          <h2>About</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-            velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia
-            nisi ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat
-            ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet
-            urna imperdiet ac. Sed nec nulla aliquam, bibendum odio eget,
-            vestibulum tortor. Cras rutrum ligula in tincidunt commodo. Morbi
-            sit amet mollis orci, in tristique ex. Donec nec ornare elit. Donec
-            blandit est sed risus feugiat porttitor. Vestibulum molestie
-            hendrerit massa non consequat. Vestibulum vitae lorem tortor. In
-            elementum ultricies tempus. Interdum et malesuada fames ac ante
-            ipsum primis in faucibus.
-          </p>
+          <img src={aboutImage} alt="a man holding sun in hands"></img>
         </div>
       </div>
     </section>
