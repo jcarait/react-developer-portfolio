ccf38ecf7565b32e204776a0ace89fc9baa88bbb
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-05-09 13:53:23 +1000

    added navigation component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,82 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Header.scss';
+import Navigation from './Navigation';
 
-function Header({ currentPage, handlePageChange }) {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
-
+function Header() {
   return (
     <header>
-      <nav className="flex-nav">
-        <div id="icon" className="column-6 column-8"></div>
-        <div className="column-6 column-4">
-          <button
-            className="hamburger"
-            onClick={() => {
-              setIsNavExpanded(!isNavExpanded);
-            }}
-          >
-            <i className="toggle fa fa-bars"></i>
-          </button>
-          <ul className={isNavExpanded ? 'nav active' : ''}>
-            <li className="nav-item">
-              <a
-                href="#home"
-                onClick={() => {
-                  handlePageChange('Home');
-                  setIsNavExpanded(false);
-                }}
-                className={
-                  currentPage === 'Home' ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#about"
-                onClick={() => {
-                  handlePageChange('About');
-                  setIsNavExpanded(false);
-                }}
-                className={
-                  currentPage === 'About' ? 'nav-link active' : 'nav-link'
-                }
-              >
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#projects"
-                onClick={() => {
-                  handlePageChange('Projects');
-                  setIsNavExpanded(false);
-                }}
-                className={
-                  currentPage === 'Projects' ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Projects
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#contact"
-                onClick={() => {
-                  handlePageChange('Contact');
-                  setIsNavExpanded(false);
-                }}
-                className={
-                  currentPage === 'Contact' ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navigation />
     </header>
   );
 }
